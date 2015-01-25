2dc5b9cc5d8b4845ad0fbe5d465dd79037505c0f
Jacob Evans <jacob@dekz.net>
2015-01-26 08:48:39 +1100

    The Matrix is cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+# This is a generic cache object which has not changed
+# much from the original cacheVector. Infact it is generic
+# and can be simply a cacheObject. The logic of what is cached
+# is contained in cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
+  solveCache <- NULL
+  get <- function() x
+  # Set the Matrix, remove any old solved data
+  set <- function(y) {
+    x <<- y
+    solveCache <<- NULL
+    x
+  }
 
+  # Set the Solve
+  setSolve <- function(y) {
+    solveCache <<- y
+    solveCache
+  }
+
+  getSolve <- function() solveCache
+
+  list(get=get, set=set, getSolve=getSolve, setSolve=setSolve)
 }
 
 
-## Write a short comment describing this function
-
+# This function contains the reposnbility of solving the inverse matrix itself
+# and updating the cache object with the result. If it is already solved
+# then simply return that value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getSolve()
+  if (!is.null(inverse)) {
+    message("Getting Cached Data")
+    ## Return a matrix that is the inverse of 'x'
+    return(inverse)
+  }
+
+  # Inverse must not yet be solved, lets solve and set it
+  # Default return here from setSolve will returned the solved value
+  x$setSolve(solve(x$get(), ...))
 }
